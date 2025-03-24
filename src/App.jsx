f58e6bb549cc8b924ff698b0bbdf252a6542929f
Supaluck Singjan <supalucksingjan@gmail.com>
2025-03-25 01:13:39 +0700

    Change className to be in 'todo-list'

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,6 @@ function App() {
 
   return (
     <div className="App">
-      <form className="addTask">
-        <label>Enter to do list
-          <input type="text" />
-          <input type="button" value="Add Task" />
-        </label>
-      </form>
-      <div className="list-todo">
-      
-      </div>
-    
       <Task />
     </div>
   )
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,5 +1,15 @@
 const Task = () => {
-    return <h1>Task</h1>
+    return <>
+    <form className="addTask">
+        <label>Enter to do list
+          <input type="text" />
+          <input type="button" value="Add Task" />
+        </label>
+      </form>
+      <div className="todo-list">
+      
+      </div>
+    </>
 }
 
 export default Task
